beb50bb63b95d78360aa40aeb9b65ae441c4701a
M-Tanzil <tanzilrazachas@gmail.com>
2026-07-15 10:35:16 +0530

    Operators

---
+++ operator.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+ a := 10
+ b := 15
+ 
+ fmt.Println("The Sum of a and b is : ",a+b)
+ fmt.Println("The Diff of a and b is :",a-b)
+ fmt.Println("The Product of a and b is :",a*b)
+ fmt.Println("The devide of a and b is : ",a/b)
+ fmt.Println("The modulas of a and b is : ",a%b)
+}
